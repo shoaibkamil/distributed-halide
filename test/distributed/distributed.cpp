382e2958fef34a1afa2e5187d81b57d709b66079
Tyler Denniston <tyler@csail.mit.edu>
2015-06-29 16:08:14 -0400

    Make distributed test simpler.

--- test/distributed/distributed.cpp
+++ test/distributed/distributed.cpp
@@ -4,19 +4,17 @@
 using namespace Halide;
 
 int main(int argc, char **argv) {
+    Var x, y;
     {
-        Func f, g;
-        Var x, y;
+        Func f;
         f(x, y) = x + y;
-        g(x, y) = f(x, y) + f(x+1, y);
         
-        f.compute_root();
-        g.compute_root().distribute(x);
+        f.compute_root().distribute(x);
 
-        Image<int> im = g.realize(10, 10);
+        Image<int> im = f.realize(10, 10);
         for (int y = 0; y < im.height(); y++) {
             for (int x = 0; x < im.width(); x++) {
-                int correct = (x + y) + ((x+1) + y);
+                int correct = x + y;
                 if (im(x, y) != correct) {
                     printf("im(%d, %d) = %d instead of %d\n", x, y, im(x, y), correct);
                     return -1;
