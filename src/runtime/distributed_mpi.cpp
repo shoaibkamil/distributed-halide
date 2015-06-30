6b1c9adf7eebe929e202d335312bef65b4a83ed4
Tyler Denniston <tyler@csail.mit.edu>
2015-06-30 10:20:45 -0400

    Remove code gen for distributed for.

--- src/runtime/distributed_mpi.cpp
+++ src/runtime/distributed_mpi.cpp
@@ -5,7 +5,15 @@
 typedef int (*halide_task)(void *user_context, int, uint8_t *);
 
 extern "C" {
-extern int printf(const char *format, ...);;
+extern int printf(const char *format, ...);
+extern double ceil(double d);
+
+typedef int MPI_Comm;
+extern int MPI_Comm_size(MPI_Comm, int *);
+extern int MPI_Comm_rank(MPI_Comm, int *);
+extern int MPI_Comm_dup(MPI_Comm comm, MPI_Comm *newcomm);
+#define MPI_COMM_WORLD ((MPI_Comm)0x44000000)
+MPI_Comm HALIDE_MPI_COMM;
 
 WEAK int halide_do_task(void *user_context, halide_task f, int idx,
                         uint8_t *closure);
@@ -14,6 +22,15 @@ WEAK int halide_do_task(void *user_context, halide_task f, int idx,
 
 namespace Halide { namespace Runtime { namespace Internal {
 
+WEAK int halide_num_processes;
+WEAK bool halide_mpi_initialized = false;
+
+WEAK void halide_initialize_mpi() {
+    MPI_Comm_dup(MPI_COMM_WORLD, &HALIDE_MPI_COMM);
+    MPI_Comm_size(HALIDE_MPI_COMM, &halide_num_processes);
+    halide_mpi_initialized = true;
+}
+
 WEAK int default_do_task(void *user_context, halide_task f, int idx,
                         uint8_t *closure) {
     return f(user_context, idx, closure);
@@ -21,12 +38,22 @@ WEAK int default_do_task(void *user_context, halide_task f, int idx,
 
 WEAK int default_do_distr_for(void *user_context, halide_task f,
                             int min, int size, uint8_t *closure) {
-    for (int x = min; x < min + size; x++) {
+    if (!halide_mpi_initialized) {
+        halide_initialize_mpi();
+    }
+    int rank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int b = (int)ceil((double)size / halide_num_processes);
+    int start = min + b*rank,
+        finish = min + b*(rank+1);
+    finish = finish <= size ? finish : size;
+    for (int x = start; x < finish; x++) {
         int result = halide_do_task(user_context, f, x, closure);
         if (result) {
             return result;
         }
     }
+    printf("After distr_for on rank %d over (%d,%d)\n", rank, start, finish);
     // Return zero if the job succeeded, otherwise return the exit
     // status.
     return 0;
--- test/distributed/distributed.cpp
+++ test/distributed/distributed.cpp
@@ -1,8 +1,23 @@
 #include "Halide.h"
 #include "mpi.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 using namespace Halide;
+
+int mpi_printf(const char *format, ...) {
+    int rank = 0, rc;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        va_list args;
+        va_start (args, format);
+        rc = vprintf (format, args);
+        va_end (args);
+        return rc;
+    } else {
+        return 0;
+    }
+}
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -12,21 +27,21 @@ int main(int argc, char **argv) {
         Func f;
         f(x, y) = x + y;
 
-        f.compute_root().distribute(x);
+        f.compute_root().distribute(y);
 
-        Image<int> im = f.realize(10, 10);
+        Image<int> im = f.realize(10, 20);
         for (int y = 0; y < im.height(); y++) {
             for (int x = 0; x < im.width(); x++) {
                 int correct = x + y;
                 if (im(x, y) != correct) {
-                    printf("im(%d, %d) = %d instead of %d\n", x, y, im(x, y), correct);
+                    mpi_printf("im(%d, %d) = %d instead of %d\n", x, y, im(x, y), correct);
                     return -1;
                 }
             }
         }
     }
 
-    printf("Success!\n");
+    mpi_printf("Success!\n");
 
     MPI_Finalize();
     return 0;
