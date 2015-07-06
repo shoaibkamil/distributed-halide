b9ff661188681c5006af43b1232bd3f4118fd561
Tyler Denniston <tyler@csail.mit.edu>
2015-07-06 12:14:33 -0400

    Refactor

--- src/DistributeLoops.cpp
+++ src/DistributeLoops.cpp
@@ -83,55 +83,62 @@ class DistributeLoops : public IRMutator {
         FindBuffersUsingVariable(string n) : name(n) {}
     };
 
-    Expr num_processors() {
+    inline Expr num_processors() const {
         return Call::make(Int(32), "halide_do_distr_size", {}, Call::Extern);
     }
 
-    Expr rank() {
+    inline Expr rank() const {
         return Call::make(Int(32), "halide_do_distr_rank", {}, Call::Extern);
+    }
+
+    // Return a new loop that has iterations determined by processor
+    // rank.
+    Stmt distribute_loop_iterations(const For *for_loop) const {
+        Var P("P"), p("p");
+        Expr slice_size = (for_loop->extent - for_loop->min) / P;
+        Expr newmin = slice_size*p, newmax = slice_size*(p+1);
+        Expr newextent = simplify(newmax - newmin);
+        // TODO: choose correct loop type here.
+        Stmt newloop = For::make(for_loop->name, newmin, newextent,
+                                 ForType::Serial, for_loop->device_api,
+                                 for_loop->body);
+        newloop = LetStmt::make(P.name(), num_processors(),
+                                LetStmt::make(p.name(), rank(), newloop));
+        return newloop;
     }
 
 public:
     using IRMutator::visit;
 
     void visit(const For *for_loop) {
-        if (for_loop->for_type == ForType::Distributed) {
-            // Find all input and output buffers in the loop body
-            // using the distributed loop variable.
-            FindBuffersUsingVariable find(for_loop->name);
-            for_loop->body.accept(&find);
-
-            map<string, Box> required, provided;
-            required = boxes_required(for_loop->body);
-            provided = boxes_provided(for_loop->body);
-
-            // Split original loop into chunks of iterations for each rank.
-            Var P("P"), p("p");
-            Expr slice_size = (for_loop->extent - for_loop->min) / P;
-            Expr newmin = slice_size*p, newmax = slice_size*(p+1);
-            Expr newextent = simplify(newmax - newmin);
-            // TODO: choose correct loop type here.
-            Stmt newloop = For::make(for_loop->name, newmin, newextent,
-                                     ForType::Serial, for_loop->device_api,
-                                     for_loop->body);
-            newloop = LetStmt::make(P.name(), num_processors(),
-                                    LetStmt::make(p.name(), rank(), newloop));
-
-            // // Send result back to root processor
-            // Expr condition = NE::make(p, 0);
-
-            // // Get address of buffer
-            // Expr first_elem = Load::make(UInt(8), find.outputs[0], 0, Buffer(), Parameter());
-            // Expr buf = Call::make(Handle(), Call::address_of, {first_elem}, Call::Intrinsic);
-            // // Get number of bytes in buffer
-            // Expr buffer_min = Call::make(UInt(8), Call::extract_buffer_min, {0}
-            // Expr send = Call::make(Int(32), "halide_do_distr_send", {buf, count, 0}, Call::Extern);
-            // Expr maybesend = IfThenElse::make(condition, send);
-
-            stmt = newloop;
-        } else {
+        if (for_loop->for_type != ForType::Distributed) {
             IRMutator::visit(for_loop);
+            return;
         }
+        // Find all input and output buffers in the loop body
+        // using the distributed loop variable.
+        FindBuffersUsingVariable find(for_loop->name);
+        for_loop->body.accept(&find);
+
+        map<string, Box> required, provided;
+        required = boxes_required(for_loop->body);
+        provided = boxes_provided(for_loop->body);
+
+        // Split original loop into chunks of iterations for each rank.
+        Stmt newloop = distribute_loop_iterations(for_loop);
+
+        // // Send result back to root processor
+        // Expr condition = NE::make(p, 0);
+
+        // // Get address of buffer
+        // Expr first_elem = Load::make(UInt(8), find.outputs[0], 0, Buffer(), Parameter());
+        // Expr buf = Call::make(Handle(), Call::address_of, {first_elem}, Call::Intrinsic);
+        // // Get number of bytes in buffer
+        // Expr buffer_min = Call::make(UInt(8), Call::extract_buffer_min, {0}
+        // Expr send = Call::make(Int(32), "halide_do_distr_send", {buf, count, 0}, Call::Extern);
+        // Expr maybesend = IfThenElse::make(condition, send);
+
+        stmt = newloop;
     }
 
 };
