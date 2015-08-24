c49742359632fcdccd06ba74017bcc327ed9e4f5
Tyler Denniston <tyler@csail.mit.edu>
2015-08-24 08:51:22 -0400

    Updates to scalability tool.

--- tools/scalability-test.py
+++ tools/scalability-test.py
@@ -11,22 +11,31 @@ class Config:
     def __init__(self, output_dir, nodes, srcfile, args):
         self.output_dir = output_dir
         self.nodes = map(int, nodes.split(","))
+        self.tasks_per_node = [1]
         self.srcfile = srcfile
         self.exe = args
+        self.input_size = "%sx%s" % (args[-2], args[-1])
         if 1 not in self.nodes:
             self.nodes = [1] + self.nodes
-        self.input_size = "%sx%s" % (args[-2], args[-1])
+        if 1 not in self.tasks_per_node:
+            self.tasks_per_node = [1] + self.tasks_per_node
 
 class Result:
-    def __init__(self, cmd, value):
+    def __init__(self, cmd, num_nodes, value):
         self.cmd = cmd
+        self.num_nodes = num_nodes
         self.value = value
 
-def make_run_cmd(config, num_nodes, tasks_per_node):
+def make_run_cmd(config, nranks, num_nodes):
     cmd = ["srun", "--exclude=lanka11", "--exclusive"]
-    cmd.extend(["--cpu_bind=verbose"])
-    cmd.extend(["--ntasks-per-node", tasks_per_node])
-    cmd.extend(["-N", num_nodes])
+    if nranks == num_nodes * 2:
+        cmd.extend(["--cpu_bind=verbose,sockets"])
+    elif nranks > num_nodes:
+        cmd.extend(["--cpu_bind=verbose,cores"])
+    else:
+        cmd.extend(["--cpu_bind=verbose"])
+    cmd.extend(["--ntasks=%d" % nranks])
+    cmd.extend(["--nodes=%d" % num_nodes])
     cmd.extend(config.exe)
     cmd = map(str, cmd)
     return cmd
@@ -64,26 +73,38 @@ def get_unique_path(template):
 def run(config):
     results = {}
     for num_nodes in config.nodes:
-        tasks_per_node = 1
-        cmd = make_run_cmd(config, num_nodes, tasks_per_node)
-        result = get_time(execute(cmd))
-        results[num_nodes] = Result(cmd, result)
+        for ntasks in config.tasks_per_node:
+            nranks = num_nodes * ntasks
+            cmd = make_run_cmd(config, nranks, num_nodes)
+            result = get_time(execute(cmd))
+            results[nranks] = Result(cmd, num_nodes, result)
     return results
 
 def calculate_slope(results):
     x = []
     y = []
-    singlenode = results[1].value
+    singlerank = results[1].value
     for k, v in results.items():
-        speedup = singlenode/v.value
+        speedup = singlerank/v.value
         x.append(k)
         y.append(speedup)
     line = np.polyfit(x, y, 1)
     return line[0]
 
+def calculate_highest_speedup(results):
+    ranks = max(results.keys())
+    singlerank = results[1].value
+    return (ranks, singlerank/results[ranks].value)
+
 def report(config, results):
     slope = calculate_slope(results)
     print "Best fit speedup slope: %.3f" % slope
+    best = calculate_highest_speedup(results)
+    print "Single rank runtime: %.3f sec" % results[1].value
+    print "%d rank runtime: %.3f" % (best[0], results[best[0]].value)
+    print "%d rank speedup: %.3f (%.1f%% of linear)" % (best[0], best[1], (best[1]/best[0])*100.0)
+    if config.output_dir == None:
+        return
     contents = ""
     contents += config.exe[0] + "\n"
     contents += "Input size: %s\n" % config.input_size
@@ -92,21 +113,33 @@ def report(config, results):
     for k, v in results.items():
         contents += " %d: %s\n" % (k, " ".join(v.cmd))
     contents += "Best fit speedup slope: %.3f\n" % slope
-    contents += "End to end times per rank count (in seconds):\n"
+    contents += "Runtime and speedup per rank count (in seconds):\n"
     contents += "--BEGIN DATA--\n"
+    singlerank = results[1].value
     for k, v in results.items():
-        contents += "%d: %.3f\n" % (k, v.value)
+        contents += "%d: %.3f\t%.3f\n" % (k, v.value, singlerank/v.value)
     contents += "--END DATA--\n"
     contents += "Raw source dump:\n"
     contents += "// File name %s\n" % config.srcfile
     contents += readall(config.srcfile)
 
     exe = os.path.basename(config.exe[0])
-    template = "%s-%s-%s-%%d.dat" % (exe, ",".join(map(str, config.nodes)),
-                                     config.input_size)
-    path = get_unique_path(os.path.join(config.output_dir, template))
-    with open(path, "w") as f:
+    txttemplate = "%s-%s-%s-%%d.txt" % (exe, ",".join(map(str, config.nodes)),
+                                        config.input_size)
+    dattemplate = "%s-%s-%s-%%d.dat" % (exe, ",".join(map(str, config.nodes)),
+                                        config.input_size)
+    txtpath = get_unique_path(os.path.join(config.output_dir, txttemplate))
+    datpath = get_unique_path(os.path.join(config.output_dir, dattemplate))
+    with open(txtpath, "w") as f:
         f.write(contents)
+    with open(datpath, "w") as f:
+        f.write("%% %s\n" % config.exe[0])
+        f.write("%% Speedup versus number of ranks (%s image)\n" % config.input_size)
+        f.write("% Img size, Number of ranks, runtime (sec), speedup:\n")
+        singlerank = results[1].value
+        for k, v in results.items():
+            speedup = singlerank / v.value
+            f.write("%s,%s,%s,%s\n" % (config.input_size, str(k), str(v.value), str(speedup)))
 
 def parse_config_argv():
     parser = OptionParser("Usage: %prog [options] exe [args]")
@@ -117,7 +150,7 @@ def parse_config_argv():
     parser.add_option("-s", "--src", dest="srcfile",
                       help="Source file for executable")
     (options, args) = parser.parse_args()
-    if (len(args) == 0 or len(options.output_dir) == 0 or len(options.nodes) == 0):
+    if (len(args) == 0 or len(options.nodes) == 0):
         parser.print_help()
         sys.exit(1)
     config = Config(options.output_dir, options.nodes,
