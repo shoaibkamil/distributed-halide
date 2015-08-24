77c1b0b024ea02012573e1b8c994190ff68b9df6
Tyler Denniston <tyler@csail.mit.edu>
2015-08-24 09:03:30 -0400

    Update blur test

--- apps/blur/halide_blur_distributed.cpp
+++ apps/blur/halide_blur_distributed.cpp
@@ -8,10 +8,6 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    timeval e2e_start, e2e_end;
-    gettimeofday(&e2e_start, NULL);
-
     const int w = std::stoi(argv[1]), h = std::stoi(argv[2]);
     Func blur_x("blur_x"), blur_y("blur_y");
     Var x("x"), y("y"), xi("xi"), yi("yi");
@@ -51,18 +47,15 @@ int main(int argc, char **argv) {
     output.set_domain(x, y);
     output.placement().split(y, y, yi, 8).distribute(y);
     output.allocate();
-    // Run the program and test output for correctness
+    // Realize once to compile
     blur_y.realize(output.get_buffer());
+    // Run the program and test output for correctness
     timeval t1, t2;
-    const int niters = 10;
+    MPI_Barrier(MPI_COMM_WORLD);
     gettimeofday(&t1, NULL);
-    for (int i = 0; i < niters; i++) {
-        blur_y.realize(output.get_buffer());
-    }
+    blur_y.realize(output.get_buffer());
     gettimeofday(&t2, NULL);
-    float sum = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0f;
-
-    //printf("[rank %d] avg time\t%.3f\tseconds\n", rank, sum/niters);
+    float compute_sec = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0f;
 
     for (int y = 0; y < output.height(); y++) {
         for (int x = 0; x < output.width(); x++) {
@@ -77,30 +70,27 @@ int main(int argc, char **argv) {
                                  ((gxm1 + gyp1 + gx + gyp1 + gxp1 + gyp1)/3)) / 3;
             if (output(x, y) != correct) {
                 printf("[rank %d] output(%d,%d) = %d instead of %d\n", rank, x, y, output(x, y), correct);
+                MPI_Abort(MPI_COMM_WORLD, 1);
                 MPI_Finalize();
                 return -1;
             }
         }
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    gettimeofday(&e2e_end, NULL);
-    float e2e_sec = (e2e_end.tv_sec - e2e_start.tv_sec) + (e2e_end.tv_usec - e2e_start.tv_usec) / 1000000.0f;
-    float total = e2e_sec;
+    float max_compute = compute_sec;
     if (rank == 0) {
         for (int i = 1; i < numprocs; i++) {
             float t;
             MPI_Recv(&t, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            total += t;
+            max_compute = t > max_compute ? t : max_compute;
         }
     } else {
-        MPI_Send(&e2e_sec, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&compute_sec, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
     }
 
     if (rank == 0) {
         printf("Blur test succeeded!\n");
-        printf("Timing: <%d> ranks <%.3f> seconds\n", numprocs, total/numprocs);
+        printf("Timing: <%d> ranks <%.3f> seconds\n", numprocs, max_compute);
     }
     MPI_Finalize();
     return 0;
