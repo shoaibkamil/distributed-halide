f86ae941ea457d29cea0a98b5c100ad3021c2167
Tyler Denniston <tyler@csail.mit.edu>
2015-09-01 11:50:50 -0400

    Fix allocation bounds determination.
    This seems to fix the bug with interpolate.

--- src/DistributedImage.cpp
+++ src/DistributedImage.cpp
@@ -12,6 +12,38 @@ namespace {
 
 class GetBoxes : public IRVisitor {
     vector<std::pair<string, Expr> > lets;
+    Scope<Interval> loop_var_bounds;
+
+    Box simplify_box(const Box &b) {
+        Box result;
+        // TODO: this can be quite slow with large
+        // environments. The goal is to get the Box into terms of
+        // the outermost variables (the global output buffer
+        // variables).
+        for (unsigned i = 0; i < b.size(); i++) {
+            Expr min = b[i].min, max = b[i].max;
+            for (auto let = lets.rbegin(); let != lets.rend(); ++let) {
+                min = simplify(substitute(let->first, let->second, min));
+                max = simplify(substitute(let->first, let->second, max));
+            }
+            result.push_back(Interval(min, max));
+        }
+        return result;
+    }
+
+    void get_bounds(Stmt s) {
+        map<string, Box> required = boxes_required(s, loop_var_bounds);
+        for (auto it : required) {
+            const auto bounds = boxes.find(it.first);
+            if (bounds == boxes.end()) {
+                boxes[it.first] = simplify_box(it.second);
+            } else {
+                Box existing = bounds->second;
+                merge_boxes(existing, it.second);
+                boxes[it.first] = simplify_box(existing);
+             }
+        }
+    }
 public:
     using IRVisitor::visit;
 
@@ -28,24 +60,11 @@ public:
     }
 
     virtual void visit(const For *op) {
-        IRVisitor::visit(op);
-        map<string, Box> r = boxes_required(op);
-        for (auto it : r) {
-            Box b;
-            // TODO: this can be quite slow with large
-            // environments. The goal is to get the Box into terms of
-            // the outermost variables (the global output buffer
-            // variables).
-            for (unsigned i = 0; i < it.second.size(); i++) {
-                Expr min = it.second[i].min, max = it.second[i].max;
-                for (auto let = lets.rbegin(); let != lets.rend(); ++let) {
-                    min = simplify(substitute(let->first, let->second, min));
-                    max = simplify(substitute(let->first, let->second, max));
-                }
-                b.push_back(Interval(min, max));
-            }
-            boxes[it.first] = b;
-        }
+        loop_var_bounds.push(op->name, Interval(op->min, op->min + op->extent - 1));
+        get_bounds(op);
+        // Don't need to recurse on the body because the required
+        // region will be redundant for nested accesses.
+        loop_var_bounds.pop(op->name);
     }
 
     map<string, Box> boxes;
