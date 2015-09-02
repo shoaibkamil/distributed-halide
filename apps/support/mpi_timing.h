198eea0664a45e65149f7908f161f762a95b237b
Tyler Denniston <tyler@csail.mit.edu>
2015-09-02 13:45:05 -0400

    Also gather 20/80th percentiles.

--- apps/support/mpi_timing.h
+++ apps/support/mpi_timing.h
@@ -21,13 +21,21 @@ public:
         MPI_Comm_size(_comm, &_numprocs);
         _usempi = true;
         _reduced = -1.0;
+        _percentile_lower = -1.0;
+        _percentile_upper = -1.0;
         _gathered = -1.0;
+        _gathered_percentile_lower = -1.0;
+        _gathered_percentile_upper = -1.0;
     }
 
     MPITiming() {
         _usempi = false;
         _reduced = -1.0;
+        _percentile_lower = -1.0;
+        _percentile_upper = -1.0;
         _gathered = -1.0;
+        _gathered_percentile_lower = -1.0;
+        _gathered_percentile_upper = -1.0;
     }
 
     inline void barrier() const {
@@ -36,13 +44,15 @@ public:
 
     void report() const {
         if (_rank == 0) {
-            printf("Timing: <%d> ranks <%f> seconds\n", _numprocs, gathered());
+            printf("Timing: <%d> ranks <%f> seconds, 20/80 percentile <%f,%f>\n",
+                   _numprocs, gathered(), _gathered_percentile_lower, _gathered_percentile_upper);
         }
     }
 
     void nondistributed_report() const {
         if (_rank == 0) {
-            printf("Timing: non-distributed <%f> seconds\n", _reduced);
+            printf("Timing: non-distributed <%f> seconds, 20/80 percentile <%f,%f>\n",
+                   _reduced, _percentile_lower, _percentile_upper);
         }
     }
 
@@ -52,6 +62,8 @@ public:
 
     // Reduce all recorded values by the given statistic.
     void reduce(Stat stat) {
+        _percentile_lower = compute_percentile(_timings, 20);
+        _percentile_upper = compute_percentile(_timings, 80);
         _reduced = compute_stat(_timings, stat);
         _timings.clear();
     }
@@ -63,15 +75,26 @@ public:
         const int tag = 0;
         if (_rank == 0) {
             vector<float> results;
+            vector<float> percentilelower, percentileupper;
             results.push_back(_reduced);
+            percentilelower.push_back(_percentile_lower);
+            percentileupper.push_back(_percentile_upper);
             for (int i = 1; i < _numprocs; i++) {
                 float t;
                 MPI_Recv(&t, 1, MPI_FLOAT, i, tag, _comm, MPI_STATUS_IGNORE);
                 results.push_back(t);
+                MPI_Recv(&t, 1, MPI_FLOAT, i, tag, _comm, MPI_STATUS_IGNORE);
+                percentilelower.push_back(t);
+                MPI_Recv(&t, 1, MPI_FLOAT, i, tag, _comm, MPI_STATUS_IGNORE);
+                percentileupper.push_back(t);
             }
             _gathered = compute_stat(results, stat);
+            _gathered_percentile_lower = compute_stat(percentilelower, Min);
+            _gathered_percentile_upper = compute_stat(percentileupper, Max);
         } else {
             MPI_Send(&_reduced, 1, MPI_FLOAT, 0, tag, _comm);
+            MPI_Send(&_percentile_lower, 1, MPI_FLOAT, 0, tag, _comm);
+            MPI_Send(&_percentile_upper, 1, MPI_FLOAT, 0, tag, _comm);
         }
     }
 
@@ -86,7 +109,9 @@ private:
     MPI_Comm _comm;
     vector<float> _timings;
     float _reduced;
+    float _percentile_lower, _percentile_upper;
     float _gathered;
+    float _gathered_percentile_lower, _gathered_percentile_upper;
 
     float compute_stat(const vector<float> &values, Stat stat) const {
         switch (stat) {
@@ -138,6 +163,34 @@ private:
             sum += v;
         }
         return sum / values.size();
+    }
+
+    float compute_percentile(const vector<float> &values, int percentile) const {
+        assert(0 < percentile && percentile <= 100);
+        if (values.size() == 1) return values[0];
+        float result = 0;
+        vector<float> tmp(values.begin(), values.end());
+        int midpoint = tmp.size() / 2;
+        std::sort(tmp.begin(), tmp.end());
+        float left = 0, right = 0;
+        unsigned k = 0;
+        for (unsigned i = 0; i < tmp.size(); i++) {
+            float rank = (100.0f/tmp.size()) * (i + 0.5);
+            if (percentile > rank) {
+                left = rank;
+            } else if (percentile < rank) {
+                right = rank;
+                k = i;
+                break;
+            } else {
+                return tmp[i];
+            }
+        }
+        if (k == tmp.size() - 1) {
+            return tmp[k];
+        }
+        result = tmp[k] + tmp.size() * ((percentile - left) / (right - left)) * (tmp[k+1] - tmp[k]);
+        return result;
     }
 
     bool eq(float a, float b) const {
