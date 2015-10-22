4a11c13aeaff87c2507b99ef2fefeb866bb23b99
Tyler Denniston <tyler@csail.mit.edu>
2015-10-22 13:50:08 -0400

    Update heat app

--- apps/heat/heat_distributed.cpp
+++ apps/heat/heat_distributed.cpp
@@ -14,35 +14,45 @@ int main(int argc, char **argv) {
     Var x("x"), y("y"), z("z"), xi("xi"), yi("yi");
 
     // Declare our input and output in global width and height.
-    DistributedImage<double> input(w, h, d);
-    DistributedImage<double> output(w, h, d);
-    input.set_domain(x, y, z);
-    input.placement().distribute(z);
-    input.allocate();
-    for (int z = 0; z < input.channels(); z++) {
-        for (int y = 0; y < input.height(); y++) {
-            for (int x = 0; x < input.width(); x++) {
-                input(x, y) = input.global(0, x) + input.global(1, y);
-            }
-        }
-    }
-    output.set_domain(x, y, z);
-    output.placement().distribute(z);
-    output.allocate();
+    DistributedImage<float> input(w, h, d);
+    DistributedImage<float> output(w, h, d);
 
     Func clamped;
     clamped(x, y, z) = input(clamp(x, 0, input.global_width() - 1),
                              clamp(y, 0, input.global_height() - 1),
                              clamp(z, 0, input.global_channels() - 1));
     Func heat3d("heat3d"), buffered;
-    Expr c0 = cast<double>(0.5f), c1 = cast<double>(-0.25f);
+    //Expr c0 = cast<double>(0.5f), c1 = cast<double>(-0.25f);
+    Expr c0 = 0.5f, c1 = -0.25f;
     buffered(x, y, z) = clamped(x, y, z);
     heat3d(x, y, z) = c0 * buffered(x, y, z) + c1 * (buffered(x-1, y, z) + buffered(x+1, y, z) +
                                                   buffered(x, y-1, z) + buffered(x, y+1, z) +
                                                   buffered(x, y, z-1) + buffered(x, y, z+1));
     // This has the effect of double-buffering the input:
-    buffered.compute_root().distribute(z);
-    heat3d.distribute(z);
+    // buffered.compute_root()
+    //     .tile(x, y, xi, yi, 8, 8).vectorize(xi).unroll(yi)
+    //     .parallel(z)
+    //     .distribute(z);
+    heat3d
+        .tile(x, y, xi, yi, 8, 8).vectorize(xi).unroll(yi)
+        .parallel(z)
+        .distribute(z);
+
+    output.set_domain(x, y, z);
+    output.placement().distribute(z);
+    output.allocate();
+
+    input.set_domain(x, y, z);
+    input.placement().distribute(z);
+    input.allocate(heat3d, output);
+
+    for (int z = 0; z < input.channels(); z++) {
+        for (int y = 0; y < input.height(); y++) {
+            for (int x = 0; x < input.width(); x++) {
+                input(x, y, z) = input.global(0, x) + input.global(1, y) + input.global(2, z);
+            }
+        }
+    }
 
     // Realize once to compile
     heat3d.realize(input.get_buffer());
@@ -50,14 +60,12 @@ int main(int argc, char **argv) {
     const int niters = 100;
     MPITiming timing(MPI_COMM_WORLD);
     timing.barrier();
-    timeval t1, t2;
-    gettimeofday(&t1, NULL);
     for (int i = 0; i < niters; i++) {
-        heat3d.realize(input.get_buffer());
+        timing.start();
+        heat3d.realize(output.get_buffer());
+        MPITiming::timing_t t = timing.stop();
+        timing.record(t);
     }
-    gettimeofday(&t2, NULL);
-    float t = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0f;
-    timing.record(t);
     timing.reduce(MPITiming::Median);
 
     timing.gather(MPITiming::Max);
