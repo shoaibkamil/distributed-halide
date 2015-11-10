e1a7f103e8d4c65c6def4e9c27760dc08752cccd
Tyler Denniston <tyler@csail.mit.edu>
2015-11-10 17:11:58 -0500

    Fluid updates

--- apps/fluid/fluid_distributed.cpp
+++ apps/fluid/fluid_distributed.cpp
@@ -31,6 +31,11 @@ const int imz  = 3;
 const int iene = 4;
 const int nc = 5;
 
+const int qu    = 2;
+const int qv    = 3;
+const int qw    = 4;
+const int qpres = 5;
+
 const double dx = (prob_hi - prob_lo)/n_cell;
 
 const double GAMMA = 1.4;
@@ -39,6 +44,7 @@ const double GAMMA = 1.4;
 int global_w = 0, global_h = 0, global_d = 0;
 Var x("x"), y("y"), z("z"), c("c");
 Func ctoprim("ctoprim"), courno_func("courno");
+Func diffterm("diffterm"), hypterm("hypterm");
 
 void init_data(DistributedImage<double> &data) {
     // XXX: make this a Halide stage as well so that we can
@@ -149,9 +155,424 @@ Func build_courno(Func Q) {
     return helper;
 }
 
+Func build_diffterm(Func Q) {
+    Expr OneThird   = Expr(1.0)/Expr(3.0);
+    Expr TwoThirds  = Expr(2.0)/Expr(3.0);
+    Expr FourThirds = Expr(4.0)/Expr(3.0);
+
+    Expr ALP = Expr( 0.8);
+    Expr BET = Expr(-0.2);
+    Expr GAM = Expr( 4.0)/Expr(105.0);
+    Expr DEL = Expr(-1.0)/Expr(280.0);
+    Expr CENTER = Expr(-205.0)/Expr(72.0);
+    Expr OFF1   = Expr(8.0)/Expr(5.0);
+    Expr OFF2   = Expr(-0.2);
+    Expr OFF3   = Expr(8.0)/Expr(315.0);
+    Expr OFF4   = Expr(-1.0)/Expr(560.0);
+    Expr dxinv = Expr(1.0 / dx);
+
+    Func difflux("difflux");
+    // Pure step handles irho case
+    difflux(x,y,z,c) = Expr(0.0);
+
+    Expr ux_calc =
+        (ALP*(Q(x+1,y,z,qu)-Q(x-1,y,z,qu))
+         + BET*(Q(x+2,y,z,qu)-Q(x-2,y,z,qu))
+         + GAM*(Q(x+3,y,z,qu)-Q(x-3,y,z,qu))
+         + DEL*(Q(x+4,y,z,qu)-Q(x-4,y,z,qu)))*dxinv;
+    Expr vx_calc =
+        (ALP*(Q(x+1,y,z,qv)-Q(x-1,y,z,qv))
+         + BET*(Q(x+2,y,z,qv)-Q(x-2,y,z,qv))
+         + GAM*(Q(x+3,y,z,qv)-Q(x-3,y,z,qv))
+         + DEL*(Q(x+4,y,z,qv)-Q(x-4,y,z,qv)))*dxinv;
+    Expr wx_calc =
+        (ALP*(Q(x+1,y,z,qw)-Q(x-1,y,z,qw))
+         + BET*(Q(x+2,y,z,qw)-Q(x-2,y,z,qw))
+         + GAM*(Q(x+3,y,z,qw)-Q(x-3,y,z,qw))
+         + DEL*(Q(x+4,y,z,qw)-Q(x-4,y,z,qw)))*dxinv;
+
+    Func loop1, ux, vx, wx;
+    // Do it this way so that ux, vx and wx are calculated in the same
+    // loop nest.
+    loop1(x, y, z) = {ux_calc, vx_calc, wx_calc};
+    ux(x, y, z) = loop1(x,y,z)[0];
+    vx(x, y, z) = loop1(x,y,z)[1];
+    wx(x, y, z) = loop1(x,y,z)[2];
+
+    Expr uy_calc =
+        (ALP*(Q(x,y+1,z,qu)-Q(x,y-1,z,qu))
+         + BET*(Q(x,y+2,z,qu)-Q(x,y-2,z,qu))
+         + GAM*(Q(x,y+3,z,qu)-Q(x,y-3,z,qu))
+         + DEL*(Q(x,y+4,z,qu)-Q(x,y-4,z,qu)))*dxinv;
+
+    Expr vy_calc =
+        (ALP*(Q(x,y+1,z,qv)-Q(x,y-1,z,qv))
+         + BET*(Q(x,y+2,z,qv)-Q(x,y-2,z,qv))
+         + GAM*(Q(x,y+3,z,qv)-Q(x,y-3,z,qv))
+         + DEL*(Q(x,y+4,z,qv)-Q(x,y-4,z,qv)))*dxinv;
+
+    Expr wy_calc =
+        (ALP*(Q(x,y+1,z,qw)-Q(x,y-1,z,qw))
+         + BET*(Q(x,y+2,z,qw)-Q(x,y-2,z,qw))
+         + GAM*(Q(x,y+3,z,qw)-Q(x,y-3,z,qw))
+         + DEL*(Q(x,y+4,z,qw)-Q(x,y-4,z,qw)))*dxinv;
+
+    Func loop2, uy, vy, wy;
+    loop2(x, y, z) = {uy_calc, vy_calc, wy_calc};
+    uy(x, y, z) = loop2(x,y,z)[0];
+    vy(x, y, z) = loop2(x,y,z)[1];
+    wy(x, y, z) = loop2(x,y,z)[2];
+
+    Expr uz_calc =
+        (ALP*(Q(x,y,z+1,qu)-Q(x,y,z-1,qu))
+         + BET*(Q(x,y,z+2,qu)-Q(x,y,z-2,qu))
+         + GAM*(Q(x,y,z+3,qu)-Q(x,y,z-3,qu))
+         + DEL*(Q(x,y,z+4,qu)-Q(x,y,z-4,qu)))*dxinv;
+
+    Expr vz_calc =
+        (ALP*(Q(x,y,z+1,qv)-Q(x,y,z-1,qv))
+         + BET*(Q(x,y,z+2,qv)-Q(x,y,z-2,qv))
+         + GAM*(Q(x,y,z+3,qv)-Q(x,y,z-3,qv))
+         + DEL*(Q(x,y,z+4,qv)-Q(x,y,z-4,qv)))*dxinv;
+
+    Expr wz_calc =
+        (ALP*(Q(x,y,z+1,qw)-Q(x,y,z-1,qw))
+         + BET*(Q(x,y,z+2,qw)-Q(x,y,z-2,qw))
+         + GAM*(Q(x,y,z+3,qw)-Q(x,y,z-3,qw))
+         + DEL*(Q(x,y,z+4,qw)-Q(x,y,z-4,qw)))*dxinv;
+
+    Func loop3, uz, vz, wz;
+    loop3(x, y, z) = {uz_calc, vz_calc, wz_calc};
+    uz(x, y, z) = loop3(x,y,z)[0];
+    vz(x, y, z) = loop3(x,y,z)[1];
+    wz(x, y, z) = loop3(x,y,z)[2];
+
+    Expr uxx = (CENTER*Q(x,y,z,qu)
+                + OFF1*(Q(x+1,y,z,qu)+Q(x-1,y,z,qu))
+                + OFF2*(Q(x+2,y,z,qu)+Q(x-2,y,z,qu))
+                + OFF3*(Q(x+3,y,z,qu)+Q(x-3,y,z,qu))
+                + OFF4*(Q(x+4,y,z,qu)+Q(x-4,y,z,qu)))*pow(dxinv,2);
+
+    Expr uyy = (CENTER*Q(x,y,z,qu)
+                + OFF1*(Q(x,y+1,z,qu)+Q(x,y-1,z,qu))
+                + OFF2*(Q(x,y+2,z,qu)+Q(x,y-2,z,qu))
+                + OFF3*(Q(x,y+3,z,qu)+Q(x,y-3,z,qu))
+                + OFF4*(Q(x,y+4,z,qu)+Q(x,y-4,z,qu)))*pow(dxinv,2);
+
+    Expr uzz = (CENTER*Q(x,y,z,qu)
+                + OFF1*(Q(x,y,z+1,qu)+Q(x,y,z-1,qu))
+                + OFF2*(Q(x,y,z+2,qu)+Q(x,y,z-2,qu))
+                + OFF3*(Q(x,y,z+3,qu)+Q(x,y,z-3,qu))
+                + OFF4*(Q(x,y,z+4,qu)+Q(x,y,z-4,qu)))*pow(dxinv,2);
+
+    Expr vyx = (ALP*(vy(x+1,y,z)-vy(x-1,y,z))
+                + BET*(vy(x+2,y,z)-vy(x-2,y,z))
+                + GAM*(vy(x+3,y,z)-vy(x-3,y,z))
+                + DEL*(vy(x+4,y,z)-vy(x-4,y,z)))*dxinv;
+
+    Expr wzx = (ALP*(wz(x+1,y,z)-wz(x-1,y,z))
+                + BET*(wz(x+2,y,z)-wz(x-2,y,z))
+                + GAM*(wz(x+3,y,z)-wz(x-3,y,z))
+                + DEL*(wz(x+4,y,z)-wz(x-4,y,z)))*dxinv;
+
+    // Update 0: imx
+    difflux(x,y,z,imx) = Expr(eta)*(FourThirds*uxx + uyy + uzz + OneThird*(vyx+wzx));
+
+    Expr vxx = (CENTER*Q(x,y,z,qv)
+                + OFF1*(Q(x+1,y,z,qv)+Q(x-1,y,z,qv))
+                + OFF2*(Q(x+2,y,z,qv)+Q(x-2,y,z,qv))
+                + OFF3*(Q(x+3,y,z,qv)+Q(x-3,y,z,qv))
+                + OFF4*(Q(x+4,y,z,qv)+Q(x-4,y,z,qv)))*pow(dxinv,2);
+
+    Expr vyy = (CENTER*Q(x,y,z,qv)
+                + OFF1*(Q(x,y+1,z,qv)+Q(x,y-1,z,qv))
+                + OFF2*(Q(x,y+2,z,qv)+Q(x,y-2,z,qv))
+                + OFF3*(Q(x,y+3,z,qv)+Q(x,y-3,z,qv))
+                + OFF4*(Q(x,y+4,z,qv)+Q(x,y-4,z,qv)))*pow(dxinv,2);
+
+    Expr vzz = (CENTER*Q(x,y,z,qv)
+                + OFF1*(Q(x,y,z+1,qv)+Q(x,y,z-1,qv))
+                + OFF2*(Q(x,y,z+2,qv)+Q(x,y,z-2,qv))
+                + OFF3*(Q(x,y,z+3,qv)+Q(x,y,z-3,qv))
+                + OFF4*(Q(x,y,z+4,qv)+Q(x,y,z-4,qv)))*pow(dxinv,2);
+
+    Expr uxy = (ALP*(ux(x,y+1,z)-ux(x,y-1,z))
+                + BET*(ux(x,y+2,z)-ux(x,y-2,z))
+                + GAM*(ux(x,y+3,z)-ux(x,y-3,z))
+                + DEL*(ux(x,y+4,z)-ux(x,y-4,z)))*dxinv;
+
+    Expr wzy = (ALP*(wz(x,y+1,z)-wz(x,y-1,z))
+                + BET*(wz(x,y+2,z)-wz(x,y-2,z))
+                + GAM*(wz(x,y+3,z)-wz(x,y-3,z))
+                + DEL*(wz(x,y+4,z)-wz(x,y-4,z)))*dxinv;
+
+    // Update 1: imy
+    difflux(x,y,z,imy) = Expr(eta)*(vxx + FourThirds*vyy + vzz + OneThird*(uxy+wzy));
+
+    Expr wxx = (CENTER*Q(x,y,z,qw)
+                + OFF1*(Q(x+1,y,z,qw)+Q(x-1,y,z,qw))
+                + OFF2*(Q(x+2,y,z,qw)+Q(x-2,y,z,qw))
+                + OFF3*(Q(x+3,y,z,qw)+Q(x-3,y,z,qw))
+                + OFF4*(Q(x+4,y,z,qw)+Q(x-4,y,z,qw)))*pow(dxinv,2);
+
+    Expr wyy = (CENTER*Q(x,y,z,qw)
+                + OFF1*(Q(x,y+1,z,qw)+Q(x,y-1,z,qw))
+                + OFF2*(Q(x,y+2,z,qw)+Q(x,y-2,z,qw))
+                + OFF3*(Q(x,y+3,z,qw)+Q(x,y-3,z,qw))
+                + OFF4*(Q(x,y+4,z,qw)+Q(x,y-4,z,qw)))*pow(dxinv,2);
+
+    Expr wzz = (CENTER*Q(x,y,z,qw)
+                + OFF1*(Q(x,y,z+1,qw)+Q(x,y,z-1,qw))
+                + OFF2*(Q(x,y,z+2,qw)+Q(x,y,z-2,qw))
+                + OFF3*(Q(x,y,z+3,qw)+Q(x,y,z-3,qw))
+                + OFF4*(Q(x,y,z+4,qw)+Q(x,y,z-4,qw)))*pow(dxinv,2);
+
+    Expr uxz = (ALP*(ux(x,y,z+1)-ux(x,y,z-1))
+                + BET*(ux(x,y,z+2)-ux(x,y,z-2))
+                + GAM*(ux(x,y,z+3)-ux(x,y,z-3))
+                + DEL*(ux(x,y,z+4)-ux(x,y,z-4)))*dxinv;
+
+    Expr vyz = (ALP*(vy(x,y,z+1)-vy(x,y,z-1))
+                + BET*(vy(x,y,z+2)-vy(x,y,z-2))
+                + GAM*(vy(x,y,z+3)-vy(x,y,z-3))
+                + DEL*(vy(x,y,z+4)-vy(x,y,z-4)))*dxinv;
+
+    // Update 2: imz
+    difflux(x,y,z,imz) = Expr(eta)*(wxx + wyy + FourThirds*wzz + OneThird*(uxz+vyz));
+
+    Expr txx = (CENTER*Q(x,y,z,6)
+                + OFF1*(Q(x+1,y,z,6)+Q(x-1,y,z,6))
+                + OFF2*(Q(x+2,y,z,6)+Q(x-2,y,z,6))
+                + OFF3*(Q(x+3,y,z,6)+Q(x-3,y,z,6))
+                + OFF4*(Q(x+4,y,z,6)+Q(x-4,y,z,6)))*pow(dxinv,2);
+
+    Expr tyy = (CENTER*Q(x,y,z,6)
+                + OFF1*(Q(x,y+1,z,6)+Q(x,y-1,z,6))
+                + OFF2*(Q(x,y+2,z,6)+Q(x,y-2,z,6))
+                + OFF3*(Q(x,y+3,z,6)+Q(x,y-3,z,6))
+                + OFF4*(Q(x,y+4,z,6)+Q(x,y-4,z,6)))*pow(dxinv,2);
+
+    Expr tzz = (CENTER*Q(x,y,z,6)
+                + OFF1*(Q(x,y,z+1,6)+Q(x,y,z-1,6))
+                + OFF2*(Q(x,y,z+2,6)+Q(x,y,z-2,6))
+                + OFF3*(Q(x,y,z+3,6)+Q(x,y,z-3,6))
+                + OFF4*(Q(x,y,z+4,6)+Q(x,y,z-4,6)))*pow(dxinv,2);
+
+    Expr divu  = TwoThirds*(ux(x,y,z)+vy(x,y,z)+wz(x,y,z));
+    Expr tauxx = Expr(2.0)*ux(x,y,z) - divu;
+    Expr tauyy = Expr(2.0)*vy(x,y,z) - divu;
+    Expr tauzz = Expr(2.0)*wz(x,y,z) - divu;
+    Expr tauxy = uy(x,y,z)+vx(x,y,z);
+    Expr tauxz = uz(x,y,z)+wx(x,y,z);
+    Expr tauyz = vz(x,y,z)+wy(x,y,z);
+
+    Expr mechwork = tauxx*ux(x,y,z) +
+        tauyy*vy(x,y,z) +
+        tauzz*wz(x,y,z) + pow(tauxy,2)+pow(tauxz,2)+pow(tauyz,2);
+
+    mechwork = Expr(eta)*mechwork
+        + difflux(x,y,z,imx)*Q(x,y,z,qu)
+        + difflux(x,y,z,imy)*Q(x,y,z,qv)
+        + difflux(x,y,z,imz)*Q(x,y,z,qw);
+
+    // Update 3: iene
+    difflux(x,y,z,iene) = Expr(alam)*(txx+tyy+tzz) + mechwork;
+
+    return difflux;
+}
+
+Func build_hypterm(Func U, Func Q) {
+    Expr OneThird   = Expr(1.0)/Expr(3.0);
+    Expr TwoThirds  = Expr(2.0)/Expr(3.0);
+    Expr FourThirds = Expr(4.0)/Expr(3.0);
+
+    Expr ALP = Expr( 0.8);
+    Expr BET = Expr(-0.2);
+    Expr GAM = Expr( 4.0)/Expr(105.0);
+    Expr DEL = Expr(-1.0)/Expr(280.0);
+    Expr dxinv = Expr(1.0 / dx);
+
+    Expr unp1 = Q(x+1,y,z,qu);
+    Expr unp2 = Q(x+2,y,z,qu);
+    Expr unp3 = Q(x+3,y,z,qu);
+    Expr unp4 = Q(x+4,y,z,qu);
+    Expr unm1 = Q(x-1,y,z,qu);
+    Expr unm2 = Q(x-2,y,z,qu);
+    Expr unm3 = Q(x-3,y,z,qu);
+    Expr unm4 = Q(x-4,y,z,qu);
+
+    Expr flux_irho_calc =
+        - (ALP*(U(x+1,y,z,imx)-U(x-1,y,z,imx))
+           + BET*(U(x+2,y,z,imx)-U(x-2,y,z,imx))
+           + GAM*(U(x+3,y,z,imx)-U(x-3,y,z,imx))
+           + DEL*(U(x+4,y,z,imx)-U(x-4,y,z,imx)))*dxinv;
+
+    Expr flux_imx_calc =
+        - (ALP*(U(x+1,y,z,imx)*unp1-U(x-1,y,z,imx)*unm1
+                + (Q(x+1,y,z,qpres)-Q(x-1,y,z,qpres)))
+           + BET*(U(x+2,y,z,imx)*unp2-U(x-2,y,z,imx)*unm2
+                  + (Q(x+2,y,z,qpres)-Q(x-2,y,z,qpres)))
+           + GAM*(U(x+3,y,z,imx)*unp3-U(x-3,y,z,imx)*unm3
+                  + (Q(x+3,y,z,qpres)-Q(x-3,y,z,qpres)))
+           + DEL*(U(x+4,y,z,imx)*unp4-U(x-4,y,z,imx)*unm4
+                  + (Q(x+4,y,z,qpres)-Q(x-4,y,z,qpres))))*dxinv;
+
+    Expr flux_imy_calc =
+        - (ALP*(U(x+1,y,z,imy)*unp1-U(x-1,y,z,imy)*unm1)
+           + BET*(U(x+2,y,z,imy)*unp2-U(x-2,y,z,imy)*unm2)
+           + GAM*(U(x+3,y,z,imy)*unp3-U(x-3,y,z,imy)*unm3)
+           + DEL*(U(x+4,y,z,imy)*unp4-U(x-4,y,z,imy)*unm4))*dxinv;
+
+    Expr flux_imz_calc =
+        - (ALP*(U(x+1,y,z,imz)*unp1-U(x-1,y,z,imz)*unm1)
+           + BET*(U(x+2,y,z,imz)*unp2-U(x-2,y,z,imz)*unm2)
+           + GAM*(U(x+3,y,z,imz)*unp3-U(x-3,y,z,imz)*unm3)
+           + DEL*(U(x+4,y,z,imz)*unp4-U(x-4,y,z,imz)*unm4))*dxinv;
+
+    Expr flux_iene_calc =
+        - (ALP*(U(x+1,y,z,iene)*unp1-U(x-1,y,z,iene)*unm1
+                + (Q(x+1,y,z,qpres)*unp1-Q(x-1,y,z,qpres)*unm1))
+           + BET*(U(x+2,y,z,iene)*unp2-U(x-2,y,z,iene)*unm2
+                  + (Q(x+2,y,z,qpres)*unp2-Q(x-2,y,z,qpres)*unm2))
+           + GAM*(U(x+3,y,z,iene)*unp3-U(x-3,y,z,iene)*unm3
+                  + (Q(x+3,y,z,qpres)*unp3-Q(x-3,y,z,qpres)*unm3))
+           + DEL*(U(x+4,y,z,iene)*unp4-U(x-4,y,z,iene)*unm4
+                  + (Q(x+4,y,z,qpres)*unp4-Q(x-4,y,z,qpres)*unm4)))*dxinv;
+
+    Func loop1("loop1");
+    loop1(x, y, z, c) = select(c == 0, flux_irho_calc,
+                               c == 1, flux_imx_calc,
+                               c == 2, flux_imy_calc,
+                               c == 3, flux_imz_calc,
+                               flux_iene_calc);
+    loop1.bound(c, 0, 5).unroll(c).compute_root();
+
+
+    unp1 = Q(x,y+1,z,qv);
+    unp2 = Q(x,y+2,z,qv);
+    unp3 = Q(x,y+3,z,qv);
+    unp4 = Q(x,y+4,z,qv);
+    unm1 = Q(x,y-1,z,qv);
+    unm2 = Q(x,y-2,z,qv);
+    unm3 = Q(x,y-3,z,qv);
+    unm4 = Q(x,y-4,z,qv);
+
+    flux_irho_calc=loop1(x,y,z,irho) -
+        (ALP*(U(x,y+1,z,imy)-U(x,y-1,z,imy))
+         + BET*(U(x,y+2,z,imy)-U(x,y-2,z,imy))
+         + GAM*(U(x,y+3,z,imy)-U(x,y-3,z,imy))
+         + DEL*(U(x,y+4,z,imy)-U(x,y-4,z,imy)))*dxinv;
+
+    flux_imx_calc=loop1(x,y,z,imx) -
+        (ALP*(U(x,y+1,z,imx)*unp1-U(x,y-1,z,imx)*unm1)
+         + BET*(U(x,y+2,z,imx)*unp2-U(x,y-2,z,imx)*unm2)
+         + GAM*(U(x,y+3,z,imx)*unp3-U(x,y-3,z,imx)*unm3)
+         + DEL*(U(x,y+4,z,imx)*unp4-U(x,y-4,z,imx)*unm4))*dxinv;
+
+    flux_imy_calc=loop1(x,y,z,imy) -
+        (ALP*(U(x,y+1,z,imy)*unp1-U(x,y-1,z,imy)*unm1
+              + (Q(x,y+1,z,qpres)-Q(x,y-1,z,qpres)))
+         + BET*(U(x,y+2,z,imy)*unp2-U(x,y-2,z,imy)*unm2
+                + (Q(x,y+2,z,qpres)-Q(x,y-2,z,qpres)))
+         + GAM*(U(x,y+3,z,imy)*unp3-U(x,y-3,z,imy)*unm3
+                + (Q(x,y+3,z,qpres)-Q(x,y-3,z,qpres)))
+         + DEL*(U(x,y+4,z,imy)*unp4-U(x,y-4,z,imy)*unm4
+                + (Q(x,y+4,z,qpres)-Q(x,y-4,z,qpres))))*dxinv;
+
+    flux_imz_calc=loop1(x,y,z,imz) -
+        (ALP*(U(x,y+1,z,imz)*unp1-U(x,y-1,z,imz)*unm1)
+         + BET*(U(x,y+2,z,imz)*unp2-U(x,y-2,z,imz)*unm2)
+         + GAM*(U(x,y+3,z,imz)*unp3-U(x,y-3,z,imz)*unm3)
+         + DEL*(U(x,y+4,z,imz)*unp4-U(x,y-4,z,imz)*unm4))*dxinv;
+
+    flux_iene_calc=loop1(x,y,z,iene) -
+        (ALP*(U(x,y+1,z,iene)*unp1-U(x,y-1,z,iene)*unm1
+              + (Q(x,y+1,z,qpres)*unp1-Q(x,y-1,z,qpres)*unm1))
+         + BET*(U(x,y+2,z,iene)*unp2-U(x,y-2,z,iene)*unm2
+                + (Q(x,y+2,z,qpres)*unp2-Q(x,y-2,z,qpres)*unm2))
+         + GAM*(U(x,y+3,z,iene)*unp3-U(x,y-3,z,iene)*unm3
+                + (Q(x,y+3,z,qpres)*unp3-Q(x,y-3,z,qpres)*unm3))
+         + DEL*(U(x,y+4,z,iene)*unp4-U(x,y-4,z,iene)*unm4
+                + (Q(x,y+4,z,qpres)*unp4-Q(x,y-4,z,qpres)*unm4)))*dxinv;
+
+
+    Func loop2("loop2");
+    loop2(x, y, z, c) = select(c == 0, flux_irho_calc,
+                               c == 1, flux_imx_calc,
+                               c == 2, flux_imy_calc,
+                               c == 3, flux_imz_calc,
+                               flux_iene_calc);
+    loop2.bound(c, 0, 5).unroll(c).compute_root();
+
+
+    unp1 = Q(x,y,z+1,qw);
+    unp2 = Q(x,y,z+2,qw);
+    unp3 = Q(x,y,z+3,qw);
+    unp4 = Q(x,y,z+4,qw);
+    unm1 = Q(x,y,z-1,qw);
+    unm2 = Q(x,y,z-2,qw);
+    unm3 = Q(x,y,z-3,qw);
+    unm4 = Q(x,y,z-4,qw);
+
+    flux_irho_calc=loop2(x,y,z,irho) -
+        (ALP*(U(x,y,z+1,imz)-U(x,y,z-1,imz))
+         + BET*(U(x,y,z+2,imz)-U(x,y,z-2,imz))
+         + GAM*(U(x,y,z+3,imz)-U(x,y,z-3,imz))
+         + DEL*(U(x,y,z+4,imz)-U(x,y,z-4,imz)))*dxinv;
+
+    flux_imx_calc=loop2(x,y,z,imx) -
+        (ALP*(U(x,y,z+1,imx)*unp1-U(x,y,z-1,imx)*unm1)
+         + BET*(U(x,y,z+2,imx)*unp2-U(x,y,z-2,imx)*unm2)
+         + GAM*(U(x,y,z+3,imx)*unp3-U(x,y,z-3,imx)*unm3)
+         + DEL*(U(x,y,z+4,imx)*unp4-U(x,y,z-4,imx)*unm4))*dxinv;
+
+    flux_imy_calc=loop2(x,y,z,imy) -
+        (ALP*(U(x,y,z+1,imy)*unp1-U(x,y,z-1,imy)*unm1)
+         + BET*(U(x,y,z+2,imy)*unp2-U(x,y,z-2,imy)*unm2)
+         + GAM*(U(x,y,z+3,imy)*unp3-U(x,y,z-3,imy)*unm3)
+         + DEL*(U(x,y,z+4,imy)*unp4-U(x,y,z-4,imy)*unm4))*dxinv;
+
+    flux_imz_calc=loop2(x,y,z,imz) -
+        (ALP*(U(x,y,z+1,imz)*unp1-U(x,y,z-1,imz)*unm1
+              + (Q(x,y,z+1,qpres)-Q(x,y,z-1,qpres)))
+         + BET*(U(x,y,z+2,imz)*unp2-U(x,y,z-2,imz)*unm2
+                + (Q(x,y,z+2,qpres)-Q(x,y,z-2,qpres)))
+         + GAM*(U(x,y,z+3,imz)*unp3-U(x,y,z-3,imz)*unm3
+                + (Q(x,y,z+3,qpres)-Q(x,y,z-3,qpres)))
+         + DEL*(U(x,y,z+4,imz)*unp4-U(x,y,z-4,imz)*unm4
+                + (Q(x,y,z+4,qpres)-Q(x,y,z-4,qpres))))*dxinv;
+
+    flux_iene_calc=loop2(x,y,z,iene) -
+        (ALP*(U(x,y,z+1,iene)*unp1-U(x,y,z-1,iene)*unm1
+              + (Q(x,y,z+1,qpres)*unp1-Q(x,y,z-1,qpres)*unm1))
+         + BET*(U(x,y,z+2,iene)*unp2-U(x,y,z-2,iene)*unm2
+                + (Q(x,y,z+2,qpres)*unp2-Q(x,y,z-2,qpres)*unm2))
+         + GAM*(U(x,y,z+3,iene)*unp3-U(x,y,z-3,iene)*unm3
+                + (Q(x,y,z+3,qpres)*unp3-Q(x,y,z-3,qpres)*unm3))
+         + DEL*(U(x,y,z+4,iene)*unp4-U(x,y,z-4,iene)*unm4
+                + (Q(x,y,z+4,qpres)*unp4-Q(x,y,z-4,qpres)*unm4)))*dxinv;
+
+    Func flux("flux");
+    flux(x, y, z, c) = select(c == 0, flux_irho_calc,
+                              c == 1, flux_imx_calc,
+                              c == 2, flux_imy_calc,
+                              c == 3, flux_imz_calc,
+                              flux_iene_calc);
+    flux.bound(c, 0, 5).unroll(c).compute_root();
+
+    return flux;
+}
+
 void build_pipeline(Func UAccessor, Func QAccessor) {
     ctoprim = build_ctoprim(UAccessor);
     courno_func  = build_courno(QAccessor);
+    diffterm = build_diffterm(QAccessor);
+    hypterm = build_hypterm(UAccessor, QAccessor);
+
+    ctoprim.compile_jit();
+    courno_func.compile_jit();
+    diffterm.compile_jit();
+    hypterm.compile_jit();
 }
 
 void ctoprim_fort(DistributedImage<double> &U, DistributedImage<double> &Q, double &courno) {
@@ -203,7 +624,9 @@ void ctoprim_fort(DistributedImage<double> &U, DistributedImage<double> &Q, doub
     // courno = max(courmx, max(courmy, max(courmz, courno)));
 }
 
-void advance(DistributedImage<double> &U, DistributedImage<double> &Q, double &dt) {
+void advance(DistributedImage<double> &U, DistributedImage<double> &Q,
+             DistributedImage<double> &D, DistributedImage<double> &F,
+             double &dt) {
     const double OneThird      = 1.0/3.0;
     const double TwoThirds     = 2.0/3.0;
     const double OneQuarter    = 1.0/4.0;
@@ -222,6 +645,11 @@ void advance(DistributedImage<double> &U, DistributedImage<double> &Q, double &d
     if (parallel_IOProcessor()) {
         std::cout << std::scientific << "dt,courno " << std::setprecision(std::numeric_limits<double>::digits10) << dt << " " << courno << "\n";
     }
+
+    // Calculate D at time N. (D = diffterm)
+    diffterm.realize(D);
+    // Calculate F at time N (N = hypterm)
+    hypterm.realize(F);
 }
 
 } // anonymous namespace
@@ -240,6 +668,7 @@ int main(int argc, char **argv) {
     // XXX: should probably make the component the innermost
     // dimension, then w,h,d.
     DistributedImage<double> U(global_w, global_h, global_d, nc), Q(global_w, global_h, global_d, 6);
+    DistributedImage<double> D(global_w, global_h, global_d, nc), F(global_w, global_h, global_d, nc);
 
     // Impose periodic boundary conditions on U and Q
     std::vector<std::pair<Expr, Expr>>
@@ -260,6 +689,10 @@ int main(int argc, char **argv) {
     U.allocate();
     Q.set_domain(x, y, z, c);
     Q.allocate();
+    D.set_domain(x, y, z, c);
+    D.allocate();
+    F.set_domain(x, y, z, c);
+    F.allocate();
 
     init_data(U);
 
@@ -268,7 +701,7 @@ int main(int argc, char **argv) {
         if (parallel_IOProcessor()) {
             std::cout << "Advancing time step " << istep << ", time = " << time << "\n";
         }
-        advance(U, Q, dt);
+        advance(U, Q, D, F, dt);
         time = time + dt;
     }
 
